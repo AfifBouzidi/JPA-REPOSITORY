253e6e080e07acead37f4d81616fa38f0bca3772
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-02-28 01:06:14 +0100

    Id Generation Using a Database Sequence

---
+++ jpa-exercise06/src/main/java/com/abouzidi/jpa/Employee.java
@@ -0,0 +1,82 @@
+package com.abouzidi.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Employee {
+
+	@SequenceGenerator(name="emp_gen",sequenceName="emp_seq")
+	@Id
+	@GeneratedValue(generator="emp_gen")
+	private long id;
+
+	private String name;
+
+	private int salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public long getSalary() {
+		return salary;
+	}
+
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+
+}
