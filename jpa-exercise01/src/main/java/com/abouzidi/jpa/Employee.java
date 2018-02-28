abd6b1672c2c10a2043056bf3b0d0368065e88e9
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-02-28 01:02:21 +0100

    Creating an Entity

---
+++ jpa-exercise01/src/main/java/com/abouzidi/jpa/Employee.java
@@ -0,0 +1,79 @@
+package com.abouzidi.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private long id;
+
+	private String name;
+
+	private int salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(long id, String name, int salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public long getSalary() {
+		return salary;
+	}
+
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+
+}
