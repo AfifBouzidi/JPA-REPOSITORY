b12e17e08d32f58b85fcfc6d2ee8d337c875b871
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-02-28 01:05:07 +0100

    Automatic Id Generation

---
+++ jpa-exercise03/src/main/java/com/abouzidi/jpa/Employee.java
@@ -0,0 +1,81 @@
+package com.abouzidi.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+
+	private String name;
+
+	private int salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public long getSalary() {
+		return salary;
+	}
+
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+
+}
